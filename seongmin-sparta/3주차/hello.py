87fc6b869c5a6ce8907878bfc2de846ed6e9c7ca
seongminis <64977431+seongminis@users.noreply.github.com>
2020-05-30 11:58:39 +0900

    Add files via upload

---
+++ seongmin-sparta/3주차/hello.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta 
+
+## 코딩 할 준비 ##
+
+target_movie = db.movies.find_one({'title':'매트릭스'})
+target_star = target_movie['star']
+
+# movies = list(db.movies.find({'star':target_star}))
+
+# for movie in movies:
+#     print(movie['title'])
+
+db.movies.update_many({'star':target_star},{'$set':{'star':0}})
